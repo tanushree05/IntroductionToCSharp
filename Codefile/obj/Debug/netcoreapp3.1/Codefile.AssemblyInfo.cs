7ed84209918d563572d1931fe76a95dd2d6112bf
tanushree05 <singhtanushree111@gmail.com>
2024-05-03 15:33:27 +0530

    added method code

--- Codefile/obj/Debug/netcoreapp3.1/Codefile.AssemblyInfo.cs
+++ Codefile/obj/Debug/netcoreapp3.1/Codefile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codefile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf36e52fab88569e496b49e32955fe21ddecd30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codefile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codefile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
