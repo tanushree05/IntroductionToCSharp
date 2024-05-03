7bf36e52fab88569e496b49e32955fe21ddecd30
tanushree05 <singhtanushree111@gmail.com>
2024-05-03 13:58:49 +0530

    added new file

---
+++ Codefile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codefile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 24;
+
+            if (a % 2 ==0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
